eb4b0e74dddaaee14b5dff4e79a78f183a719af3
rjindalrohit <r.jindalrohit@gmail.com>
2020-03-17 16:15:40 +0530

    Create Quality_Gate_build_break.sh

---
+++ Quality_Gate_build_break.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#1) Call the /api/ce/task?id=${ceTaskId} web service to retrieve analysisId. You can get the ceTaskId from /report-task.txt under your working directory.
+#2) If the CE Task Status is PENDING or IN_PROGRESS, the script should wait, and repeat step 1
+#3) If the CE Task Status is SUCCESS, we save the analysisId and proceed to step 5
+#4) If the CE Task Status is FAILED or CANCELED, we break the build
+#5) Call the /api/qualitygates/project_status?analysisId=${analysisId} web service to check the status of the quality gate
+#6) If the quality gate status is OK or WARN, allow the build to pass. If the quality gate status is ERROR, we break the build.
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+sleep 5
+
+source ./.scannerwork/report-task.txt
+
+echo "Quality_Gate -> Sonar Project URL: $dashboardUrl"
+echo "Quality_Gate -> Project Key: $projectKey"
+
+if [ -z ${ceTaskId} ]
+then
+	echo "Quality_Gate -> No Task Id found"
+fi
+
+wait_for_success=true
+count=0
+max=5
+while [[ "${wait_for_success}" = "true" && ${count} -lt ${max} ]]
+do
+	ceTaskStatus=$(curl -s ${ceTaskUrl} | jq -r .task.status)
+    if [ "${ceTaskStatus}" = "PENDING" ] || [ "${ceTaskStatus}" = "IN_PROGRESS" ] ; then
+    	echo "Quality_Gate -> Task Id ${ceTaskId} status is : ${ceTaskStatus} -- waiting 20 Sec before Next Check"
+        sleep 20
+        count=$((count + 1))
+    fi
+    if [ "${ceTaskStatus}" = "SUCCESS" ]; then
+    	echo "Quality_Gate -> Task Id ${ceTaskId} status is : ${ceTaskStatus}"
+        wait_for_success=false
+    fi
+    
+done
+
+analysisId=$(curl -s ${ceTaskUrl} |  /home/pawan.kumar/jq/jq -r .task.analysisId)
+echo "Quality_Gate -> Analysis ID is: $analysisId"
+
+QG_Status=$(curl -s ${serverUrl}/api/qualitygates/project_status?analysisId="${analysisId}" |  jq -r .projectStatus.status)
+if [ "${QG_Status}" != "OK" ]; then
+	echo "Quality_Gate -> Quality Gate is Not OK. Breaking the build"
+    exit 1
+fi
